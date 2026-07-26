62066da3444f9f796d2d36942301a94202255477
TimDev14 <njagi.timothy14@gmail.com>
2026-07-26 11:18:37 +0300

    added the folder structure for the app and installed relevant dependencies

---
+++ server/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from app import db, ma, jwt
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
+app = Flask(__name__)
+
+# Configure db, secret key
+
+app.config['SQLALCHEMY_DATABASE_URI'] = None
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = None
+
+# Initialise db, ma, jwt
+db.init_app(app)
+ma.init_app(app)
+jwt.init_app(app)
+
+# Initialise migrate
+
+migrate = Migrate (db, app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
