d8a5b24bc2d9e715bc5f7226d3e7bf22a7e6a1b9
TimDev14 <njagi.timothy14@gmail.com>
2026-07-28 15:58:01 +0300

    feat: update associations to replace category table with tags for transactions

--- server/app/models/associations.py
+++ server/app/models/associations.py
@@ -1,7 +1,7 @@
 from extensions import db
 
-transactions_category = db.Table(
-    'transactions_category',
-    db.Column('transaction_id', db.Integer, db.ForeignKey('transactions_id'), primary_key=True),
-    db.Column('tags_id', db.Integer, db.ForeignKey('tags_id'), primary_key=True)
+transactions_tags = db.Table(
+    'transactions_tags',
+    db.Column('transaction_id', db.Integer, db.ForeignKey('transactions.id'), primary_key=True),
+    db.Column('tags_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
 )
