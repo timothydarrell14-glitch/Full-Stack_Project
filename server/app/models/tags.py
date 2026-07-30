513b839d3e99d542e2b8145b7ab0b71e34c22829
TimDev14 <njagi.timothy14@gmail.com>
2026-07-30 12:12:35 +0300

    feat: add transactions relationship to Tag model and update Transaction model to reflect changes

--- server/app/models/tags.py
+++ server/app/models/tags.py
@@ -5,3 +5,4 @@ class Tag(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    transactions = db.relationship('Transaction', back_populates='tag')
--- server/app/models/transactions.py
+++ server/app/models/transactions.py
@@ -8,5 +8,5 @@ class Transaction(db.Model):
     user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
     amount=db.Column(db.Float, nullable=False)
     date=db.Column(db.Date, nullable=False)
-    tag_id=db.Column(db.Integer, db.ForeignKey('tags.id'))
+    tag = db.relationship('Tag', back_populates='transactions', lazy=True)
     user = db.relationship('User', back_populates='transactions', lazy =True)
