e0e180908fc124f7d9df21488e3b6c1149bd6978
TimDev14 <njagi.timothy14@gmail.com>
2026-08-03 03:52:33 +0300

    fix: update import paths for Dashboard and SideBar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,11 @@ import LogInPage from './pages/Authentication/LogInPage'
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard/Dashboard'
-import OverviewPage from './pages/OverviewPage'
-import ExpensesPage from './pages/ExpensesPage'
-import SavingsPage from './pages/SavingsPage'
-import SettingsPage from './pages/SettingsPage'
-import Profile from './pages/Profile'
+import OverviewPage from './pages/Dashboard/OverviewPage'
+// import ExpensesPage from './pages/Dashboard/ExpensesPage'
+// import SavingsPage from './pages/Dashboard/SavingsPage'
+// import SettingsPage from './pages/Dashboard/SettingsPage'
+// import Profile from './components/Profile'
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
 
       <Route element={<Dashboard />}>
         <Route path="/dashboard" element={<OverviewPage />} />
-        <Route path="/expenses" element={<ExpensesPage />} />
+        {/* <Route path="/expenses" element={<ExpensesPage />} />
         <Route path="/savings" element={<SavingsPage />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} /> */}
       </Route>
 
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
